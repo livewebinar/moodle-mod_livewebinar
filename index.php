728b66afc4b76a9fa5c3f847bda1b7692ab5b2e9
Gulios <gulios@gulios.pl>
2026-01-19 12:23:13 +0100

    Moodle requirements

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-// This file is part of the livewebinar plugin for Moodle - http://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,18 +14,19 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Defines the version and other meta-info about the plugin.
- *
- * @package    mod_livewebinar
- * @copyright  LiveWebinar by RTCLAB Sp. z o.o.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-defined('MOODLE_INTERNAL') || die();
+require(__DIR__ . '/../../config.php');
 
-$plugin->component = 'mod_livewebinar';
-$plugin->version = 2026011900; //2017111300
-$plugin->release = '1.0.1';
-$plugin->requires = 2022112800; //2017111300
-$plugin->maturity = MATURITY_STABLE;
-$plugin->cron = 0;
+$id = optional_param('id', 0, PARAM_INT);
+
+if ($id) {
+    redirect(new moodle_url('/mod/livewebinar/view.php', ['id' => $id]));
+}
+
+$PAGE->set_url('/mod/livewebinar/index.php');
+$PAGE->set_context(context_system::instance());
+$PAGE->set_title(get_string('pluginname', 'mod_livewebinar'));
+$PAGE->set_heading(get_string('pluginname', 'mod_livewebinar'));
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('pluginname', 'mod_livewebinar'));
+echo $OUTPUT->footer();
